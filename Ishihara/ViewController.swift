6b32e19ab611280661b510ebcca24af7ace6438e
masterUNG <phrombutr@gmail.com>
2016-06-22 15:07:52 +0700

    get Indx

--- Ishihara/ViewController.swift
+++ Ishihara/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UIViewController {
     //Explicit
     var strAnswer:String = ""
     var intIndex:Int = 0
+    var arrayImage = ["ishihara_01.png", "ishihara_02.png", "ishihara_03.png", "ishihara_04.png", "ishihara_05.png", "ishihara_06.png", "ishihara_07.png", "ishihara_08.png", "ishihara_09.png", "ishihara_10.png"]
+    
     
     
  
@@ -38,8 +40,14 @@ class ViewController: UIViewController {
         strAnswer = String(answerTextField.text)
         print("strAnswer ==> \(strAnswer)")
         
-        intIndex += 1
-        print("intIndex ==> \(intIndex)")
+        if (intIndex < 9) {
+            intIndex += 1
+        }else {
+            intIndex = 0
+        }
+        print("intIndex ===> \(intIndex)" )
+        
+        
         
     }   // answerButton
     
